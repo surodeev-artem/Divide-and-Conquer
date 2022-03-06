0218df92f97d06f4639b292889988c2146337889
surodeev-artem <surodeev2003@mail.ru>
2022-03-06 21:57:38 +0600

    Added recursive algorithm

--- src/com/artem/surodeev/Main.java
+++ src/com/artem/surodeev/Main.java
@@ -1,8 +1,29 @@
 package com.artem.surodeev;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+        System.out.println(recursiveSearch(array, 6));
+    }
+
+    private static int recursiveSearch(int[] array, int searchElement) {
+        int countOfElements = array.length;
+        int indexOfMiddleElement = (int)countOfElements/2;
+        int middleElement = array[indexOfMiddleElement];
+        if (searchElement == middleElement) {
+            return middleElement;
+        } else if (searchElement > middleElement) {
+            return recursiveSearch(Arrays.copyOfRange(array, indexOfMiddleElement + 1, countOfElements), searchElement);
+        } else if (searchElement < middleElement) {
+            return recursiveSearch(Arrays.copyOfRange(array, 0, indexOfMiddleElement), searchElement);
+        } else {
+            return -1;
+        }
     }
 }
